a15cfcdd9b0046795b60498099729d3c51c7c654
zhaoke <zhaoke@juntu.com>
2017-05-24 17:55:30 +0800

    last

--- src/main.js
+++ src/main.js
@@ -3,7 +3,15 @@
 import Vue from 'vue'
 import layout from '@/components/layout'
 // import Vuex from 'vuex'
+
 import indexPage from '@/components/pages/indexPage'
+import detailPage from '@/components/pages/detailPage'
+import orderList from '@/components/pages/orderList'
+import detailFor from '@/components/pages/detail/forecast'
+import detailAna from '@/components/pages/detail/analysis'
+import detailCou from '@/components/pages/detail/count'
+import detailPub from '@/components/pages/detail/publish'
+
 import VRouter from 'vue-router'
 import Vueresource from 'vue-resource'
 // import Hello2 from '@/components/Hello2'
@@ -18,6 +26,33 @@ let router = new VRouter({
     {
       path: '/',
       component: indexPage
+    },
+    {
+      path: '/detail',
+      component: detailPage,
+      redirect: '/detail/count',
+      children: [
+        {
+          path: 'forecast',
+          component: detailFor
+        },
+        {
+          path: 'analysis',
+          component: detailAna
+        },
+        {
+          path: 'count',
+          component: detailCou
+        },
+        {
+          path: 'publish',
+          component: detailPub
+        }
+      ]
+    },
+    {
+      path: '/orderList',
+      component: orderList
     }
   ]
 })
